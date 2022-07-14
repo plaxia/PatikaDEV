e74a9d1ec0899fc836462d83b1ddde0e23cbab5e
Berke Ayyıldız <83032190+plaxia@users.noreply.github.com>
2022-07-14 16:02:07 +0300

    Add files via upload

---
+++ Java/PalindromeNumbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PalindromeNumbers {
+
+    static boolean isPalindrome (int number){
+        int reverse=0,n1=number,n2;
+        while(n1!=0){
+//141
+           n2=n1%10;
+            reverse=(reverse*10)+n2;
+            n1=n1/10;
+        }
+        if(reverse==number){
+            System.out.println("Palindrome");
+            return true;
+        }
+else {
+            System.out.println("Not palindrome");
+    return false ;
+        }
+    }
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer :");
+        int n1= input.nextInt(), n2;
+        int reverse = 0;
+
+
+    isPalindrome(n1);
+
+    }
+}
